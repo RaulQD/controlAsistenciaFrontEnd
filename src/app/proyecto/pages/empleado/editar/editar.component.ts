60bf61d70cc6e62e99df80ac646474ec094d81e6
RaulQD <raul.g.quispe@gmail.com>
2022-12-28 01:09:08 -0500

    crear carpeta validators

--- src/app/proyecto/pages/empleado/editar/editar.component.ts
+++ src/app/proyecto/pages/empleado/editar/editar.component.ts
@@ -6,6 +6,9 @@ import Swal from 'sweetalert2';
 import { Cargo } from '../../../interface/cargo.interface';
 import { Area } from '../../../interface/area.interface';
 import { UtilService } from '../../../service/util.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {  debounceTime } from 'rxjs';
+import { nombreApellidoPattern, dniPattern, emailPattern } from '../../../../shared/validators/validator';
 
 @Component({
   selector: 'app-editar',
@@ -13,6 +16,8 @@ import { UtilService } from '../../../service/util.service';
   styleUrls: ['./editar.component.css']
 })
 export class EditarComponent implements OnInit{
+
+
 
   cargos:Cargo[] =[];
   areas: Area[] =[];
@@ -35,46 +40,59 @@ export class EditarComponent implements OnInit{
       idCargo: -1,
     }
   }
+  form: FormGroup = this.formBuilder.group({
+    nombre: ['',[Validators.required, Validators.pattern(nombreApellidoPattern)]],
+    apellido: ['',[Validators.required, Validators.pattern(nombreApellidoPattern)]],
+    dni: ['',[Validators.required, Validators.pattern(dniPattern)]],
+    correo: ['',[Validators.required, Validators.pattern(emailPattern)]],
+    contacto: ['',[Validators.required, Validators.pattern('[0-9]{9}')]],
+    direccion: ['',[Validators.required]],
+    tarifa:[0,[Validators.required,Validators.min(0),Validators.pattern('[0-9]{1,9}')]],
+    nacimiento: ['',[Validators.required]],
+    registro:['',[Validators.required]],
+    area: ['',[Validators.required, Validators.min(1)]],
+    cargo: ['',[Validators.required, Validators.min(1)]],
+    estado:[1,[Validators.required]]
+  });
   constructor(private utilsService: UtilService ,
             private empleadoService:EmpleadoService, 
             private router:Router, 
-            private activateRouter: ActivatedRoute){
+            private activateRouter: ActivatedRoute,
+            private formBuilder: FormBuilder){
   this.utilsService.getCargo().subscribe((res) => this.cargos = res );
 
   this.utilsService.getArea().subscribe((res)=>{this.areas = res});
 }
   ngOnInit(): void{
+    this.form.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe(value =>{
+      console.log(value);
+    });
+
    this.id = this.activateRouter.snapshot.params[ 'id' ];
    this.empleadoService.getEmpleadoById(this.id).subscribe((res)=>{
      this.objEmpleado = res;
    },(error)=>{console.log(error)});
   }
-  putEmpleado(){
+  isValid(campo:string){
+    return this.form.controls[campo].hasError('required');
+  }
+  isValidPattern(campo:string){
+    return this.form.controls[campo].hasError('pattern');
+  }
+  putEmpleado(e:Event){
+    e.preventDefault();
+    if(this.form.invalid){
+      this.form.markAllAsTouched();
+      return;
+    }
     this.empleadoService.putEmpleado(this.id,this.objEmpleado).subscribe((res )=>{
       Swal.fire({title:'Actualización',text: res.errores, icon:'info'}).then(() =>{
         this.router.navigate(['/dashboard/listar-empleado']);
       });
     });
-
-    //LIMPIAR CAMPOS
-    this.objEmpleado={
-      idEmpleado: 0,
-      nombre: '',
-      apellido: '',
-      dni: '',
-      correo: '',
-      contacto: '',
-      direccion: '',
-      tarifa_hora: 0,
-      fechaNacimiento: new Date(),
-      fechaRegistro : new Date(),
-      area: {
-        idArea: -1,
-    },
-      cargo: {
-        idCargo: -1,
-      }
-    }
+    this.form.reset();
   }
   
   cancelar(){
--- src/app/proyecto/pages/empleado/registrar/registrar.component.ts
+++ src/app/proyecto/pages/empleado/registrar/registrar.component.ts
@@ -8,6 +8,7 @@ import { Area } from '../../../interface/area.interface';
 import Swal from 'sweetalert2';
 import { debounceTime} from 'rxjs/operators';
 import { FormGroup,FormBuilder,FormControl, Validators } from '@angular/forms';
+import { dniPattern, emailPattern, nombreApellidoPattern } from 'src/app/shared/validators/validator';
 
 @Component({
   selector: 'app-registrar',
@@ -21,7 +22,6 @@ export class RegistrarComponent implements OnInit {
   cargos:Cargo[] =[];
   areas: Area[] =[];
   objEmpleado: Empleado = {
-    idEmpleado: 0,
     nombre: '',
     apellido: '',
     dni: '',
@@ -37,11 +37,13 @@ export class RegistrarComponent implements OnInit {
       idCargo: -1,
     }
   }
+
+  //VALIDAR FORMULARIO FORMGROUP
   form: FormGroup = this.formBuilder.group({
-      nombre: ['',[Validators.required, Validators.pattern('[a-zA-Z0-9ñáéíóúüÁÉÍÓÚÜ ]{3,40}')]],
-      apellido: ['',[Validators.required, Validators.pattern('[a-zA-Z0-9ñáéíóúüÁÉÍÓÚÜ ]{3,40}')]],
-      dni: ['',[Validators.required, Validators.pattern('[0-9]{8}')]],
-      correo: ['',[Validators.required, Validators.email]],
+      nombre: ['',[Validators.required, Validators.pattern(nombreApellidoPattern)]],
+      apellido: ['',[Validators.required, Validators.pattern(nombreApellidoPattern)]],
+      dni: ['',[Validators.required, Validators.pattern(dniPattern)]],
+      correo: ['',[Validators.required, Validators.pattern(emailPattern)]],
       contacto: ['',[Validators.required, Validators.pattern('[0-9]{9}')]],
       direccion: ['',[Validators.required]],
       tarifa:[0,[Validators.required,Validators.min(0),Validators.pattern('[0-9]{1,9}')]],
@@ -61,21 +63,25 @@ export class RegistrarComponent implements OnInit {
     this.utilsService.getArea().subscribe((res)=>{this.areas = res});
   }
   ngOnInit(){
+    //CAMBIAR REACTIVAMENTE EL VALOR DEL FORMULARIO
     this.form.valueChanges.pipe(
       debounceTime(500)
     ).subscribe(value =>{
       console.log(value);
     });
   }
+  //SI ES REQUERIDO
   isValid(campo:string){
     return this.form.controls[campo].hasError('required');
   }
+  //SI TIENE UN PATRON ESPECIFICO
   isValidPattern(campo:string){
     return this.form.controls[campo].hasError('pattern');
   }
-  
+  //REGISTRAR EMPLEADO
   postEmpleado(e:Event){
     e.preventDefault();
+    //VALIDAR BOTON ACTIVO Y ACTIVAR LAS VALIDACIONES
     if(this.form.invalid){
       this.form.markAllAsTouched();
       return;
